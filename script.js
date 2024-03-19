99caa54ded96a22efe9abcd68de129c073d3563a
SebbeHN <163452550+SebbeHN@users.noreply.github.com>
2024-03-19 18:21:46 +0100

    kodning 1

---
+++ script.js
@@ -0,0 +1,68 @@
+/*
+alert(123) 
+document.write(456) 
+*/
+
+//console.log(789)
+
+// console.clear() resnar konsolen
+let seb 
+// undefined är tomt
+// value är också text samt siffror i
+seb = 888  //tildelning sätter in värden
+
+
+seb = "hello world"
+
+seb = seb
+
+console.log(seb)
+
+let bar = 111 //deklaration + tilldelning
+
+bar = seb 
+
+let malmo = seb 
+
+bar = 1 + 1 
+
+bar  = (2 + 2) * 3
+
+bar = "hej" + "san" //konkatination
+
+bar = "hej" + 1
+
+bar = "hej" + (1 + 1) 
+
+bar = 1 + 1 +"hej"
+
+//(2)Datatyper
+
+let x = true 
+          //number
+x = -5    // integer / heltal
+x = 1.5   // float , double / decimaltal
+
+
+          // String
+x = "hej"  // string / text
+x = '@'    // character / karaktär, tecken
+
+           // boolean
+x = true   // boolean, bool / boolean
+
+           // undefined
+x = undefined  // undefined / odefinerat  (tomt)
+
+console.log(typeof x);
+
+// Number + number = Number
+// NUMBER + BOOLEAN = NUMBER
+
+// STRING + STRING = STRING
+// STRING + NUMBER = String
+// STRING + BOOLEAN = STRING
+
+// BOOLEAN + BOOLEAN = NUMBER
+
+
